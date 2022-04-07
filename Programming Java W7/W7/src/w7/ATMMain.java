8d3edff0d9b4117d28aaadda0d930dd943fe460b
Rekabriyan <95167399+Rekabriyan@users.noreply.github.com>
2022-04-07 08:25:26 +0700

    Add files via upload

---
+++ Programming Java W7/W7/src/w7/ATMMain.java
@@ -0,0 +1,15 @@
+package w7;
+
+ 
+
+public class ATMMain {
+   // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+   }
+} 
+
+
+
+ 
